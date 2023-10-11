66ec66a483a5582372e29db6b4cfac50bd175093
Wajahat Baloch <wajahatbaloch2002@gmail.com>
2023-10-11 11:38:35 +0500

    fix css

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -15,7 +15,7 @@ const WeatherApp = () => {
       .get(
         `https://api.openweathermap.org/data/2.5/weather?q=${input}&units=Metric&appid=${api_key}`
       )
-      .then((response) => setData(response.data)); // Use response.data to set the state
+      .then((response) => setData(response.data));
   };
 
   const getWeatherByLocation = (latitude, longitude) => {
@@ -42,66 +42,56 @@ const WeatherApp = () => {
   };
 
   return (
-    <div className="grid h-screen place-items-center">
-      <div>
-        <h1 className="text-6xl">Weather App</h1>
+    <div className="grid h-screen place-items-center bg-slate-800">
+      <div className="text-center">
+        <h1 className="text-6xl font-bold">Weather App</h1>
       </div>
-      <div className="flex flex-col md:min-h-[450px] items-center w-[93%] h-[60vh] bg-slate-600 md:w-[380px] rounded-xl">
-        <div className="flex mt-8 px-2 w-[92%]">
-          <div>
-            <input
-              type="search"
-              value={input}
-              onChange={(e) => setInput(e.target.value)}
-              placeholder="Search"
-              className="text-slate-700 text-xl py-3 font-bold px-4 bg-slate-200 border-none outline-none rounded-[50px]"
-            />
-          </div>
-          <div>
-            <img
-              src={searchImage}
-              alt=""
-              onClick={handleSearch}
-              className="p-3 cursor-pointer bg-slate-200 rounded-[50px] ml-2 mt-1 hover:shadow"
-            />
-          </div>
+      <div className="flex flex-col md:min-h-[450px] items-center w-[93%] h-[60vh] bg-slate-600 md:w-[380px] rounded-xl mt-8 p-4">
+        <div className="flex w-full">
+          <input
+            type="search"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="Search"
+            className="text-slate-700 text-xl py-3 w-full font-bold px-4 bg-slate-200 border-none outline-none rounded-full"
+          />
+          <img
+            src={searchImage}
+            alt="Search"
+            onClick={handleSearch}
+            className="p-3 cursor-pointer bg-slate-200 rounded-full ml-2 mt-1 hover:shadow"
+          />
         </div>
 
-        {/* cloud */}
-        <div>
-          <img src={cloud} alt="" className="md:h-28 mt-2 h-24" />
+        <div className="text-center mt-2">
+          <img src={cloud} alt="Cloud" className="md:h-28 h-24" />
         </div>
 
-        {/* temp */}
-        <div className="text-6xl font-bold">
+        <div className="text-6xl font-bold text-white">
           {Math.floor(data?.main?.temp)}℃
         </div>
 
-        {/* location */}
-        <div className="text-2xl m-3 font-bold">{data?.name}</div>
+        <div className="text-2xl m-3 font-bold text-white">{data?.name}</div>
 
-        {/* con */}
-        <div className="flex md:mt-11 mt-[7%]">
-          <div className="flex">
-            <img src={humidity} alt="" className="h-12 p-2 mr-1 ml-2" />
+        <div className="flex mt-3">
+          <div className="flex items-center">
+            <img src={humidity} alt="Humidity" className="h-12 p-2 mr-1 ml-2" />
             <div className="text-center">
-              {/* humidity */}
               <div>{data?.main?.humidity}</div>
               <div>Humidity</div>
             </div>
           </div>
 
-          <div className="flex">
-            <img src={wind} alt="" className="h-12 p-2 mr-1 ml-4" />
+          <div className="flex items-center ml-4">
+            <img src={wind} alt="Wind Speed" className="h-12 p-2 mr-1 ml-2" />
             <div className="text-center">
-              {/* humidity */}
               <div>{data?.wind?.speed} km/h</div>
               <div>Wind Speed</div>
             </div>
           </div>
         </div>
       </div>
-      <footer className="text-2xl text-center py-4 text-white">
+      <footer className="text-2xl text-center text-white mt-4">
         &copy; {new Date().getFullYear()} Wajahat Baloch. All rights reserved.
       </footer>
     </div>
